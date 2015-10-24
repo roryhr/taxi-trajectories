301f00145ef2d6942330c78c245d5eb814e0648e
Rory H.R <roryhr@gmail.com>
2015-10-24 10:57:14 -0500

    Add .gitignore and readme

---
+++ README.md
@@ -0,0 +1,23 @@
+## T-Drive Data Challenge 
+
+http://startup.ml/challenge/
+
+In Beijing, Microsoft collected GPS data from 10,357 taxis. 
+Sampling each taxi's location every 177 seconds. 
+
+
+http://research.microsoft.com/apps/pubs/?id=152883
+
+
+# Publications
+
+[1] Jing Yuan, Yu Zheng, Xing Xie, and Guangzhong Sun. Driving with knowledge
+from the physical world. In The 17th ACM SIGKDD international conference on
+Knowledge Discovery and Data mining, KDD'11, New York, NY, USA, 2011. ACM.
+
+[2] Jing Yuan, Yu Zheng, Chengyang Zhang, Wenlei Xie, Xing Xie, Guangzhong Sun, and Yan Huang. T-drive: driving directions based on taxi trajectories. In
+Proceedings of the 18th SIGSPATIAL International Conference on Advances in
+Geographic Information Systems, GIS '10, pages 99-108, New York, NY, USA,2010.
+
+
+
--- taxis-read.py
+++ taxis-read.py
@@ -10,6 +10,25 @@ import numpy as np
 import os 
 
 
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
 def get_files(direc):
     for root, dirs, files in os.walk(direc):
         files = files
@@ -21,25 +40,42 @@ def get_files(direc):
     return full_files
     
     
-full_files = get_files('01')
+full_files = get_files('data')
 
-# Read in the data assign
+#%% Read in the data 
 frames = []
 for index, file_path in enumerate(full_files):
     data = pd.read_csv(file_path, infer_datetime_format=True,\
             header=None, parse_dates = [1],\
             names = ['taxi_id', 'date_time', 'longitude', 'latitude'])
     frames.append(data)
+    print file_path
+    print data.describe()
+
+    
     
 data = pd.concat(frames)
 del frames, index
 
-time_diffs = []
+grouped = data.groupby('taxi_id')['date_time']
+
+
+#%% Compute Time Intervals
+
+times = []
 for g in grouped:
-    time_diffs.append(g[1].diff()/np.timedelta64(1,'s'))
+    times.append(g[1].diff())
+#    print pd.Series(g[1].diff())
+#    time_diffs.append(pd.Series(g[1].diff()))
     
-    
-#x = data.groupby(by='taxi_id').date_time.diff()
+time_diffs = pd.Series()
+time_diffs = pd.concat(times)
+time_diffs /= np.timedelta64(1,'s') # Divide by 1 second, for float64 data
+
+time_diffs.dropna()
+
+time_diffs /= 60    # Convert to minutes
+time_diffs[(time_diffs > 0) & (time_diffs < 12)].hist(bins = 20)#x = data.groupby(by='taxi_id').date_time.diff()
 #data = pd.read_csv('01/9754.txt', infer_datetime_format=True,\
 #            header=None, parse_dates = [1],\
 #            names = ['taxi_id', 'date_time', 'longitude', 'latitude'])
