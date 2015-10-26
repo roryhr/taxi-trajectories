49b5c0166c78e47454f1d267934dc09b1800ea8a
Rory H.R <roryhr@gmail.com>
2015-10-26 14:55:24 -0500

    Add notebook, finish Kalman Filter

--- taxi-trajectories.py
+++ taxi-trajectories.py
@@ -4,6 +4,7 @@ Created on Fri Oct 23 19:02:14 2015
 
 @author: Rory H.R. 
 """
+from pykalman import KalmanFilter
 
 import pandas as pd
 import numpy as np
@@ -50,7 +51,7 @@ def my_low_pass_filter(array):
     half_way = int(freqs.size/2)
     ten_percent = int(freqs.size/10)
     
-    freqs[(half_way-ten_percent):(half_way+ten_percent)]
+    freqs[(half_way-2*ten_percent):(half_way+2*ten_percent)] = 0
     return np.fft.ifft(freqs)
 
 
@@ -152,33 +153,23 @@ window = data[(xmin < data.longitude) & (data.longitude < xmax) & \
 x = np.array(window.longitude)
 y = np.array(window.latitude)
 
-#plt.subplots_adjust(hspace=0.5)
-#plt.subplot(121)
-#plt.hexbin(x,y, cmap=plt.cm.YlOrRd_r)
-#plt.axis([xmin, xmax, ymin, ymax])
-#plt.title("Hexagon binning")
-#cb = plt.colorbar()
-#cb.set_label('counts')
-
-#%% Make the plot
-#plt.subplot(122)
-plt.figure(figsize = (12,8), dpi=100)
+plt.figure(figsize = (10,7), dpi=200)
 plt.hexbin(x,y,bins='log', gridsize=800, cmap=plt.cm.hot)   # black -> red > white
 plt.axis([xmin, xmax, ymin, ymax])
-plt.title("Traffic data over for Beijing")
-cb = plt.colorbar(format=LogFormatterHB())
-#cb = plt.colorbar()
+plt.title("Traffic data over Beijing")
+plt.xlabel('Longitude (degrees)')
+plt.ylabel('Latitude (degrees)')
 
+cb = plt.colorbar(format=LogFormatterHB())
 cb.set_label('Number of points')
 
 plt.show()
 
 #%% Make the 5th Ring Road Beijing
-#plt.subplot(122)
-xmin = 116.25
-xmax = 116.5
-ymin = 39.75
-ymax = 40.1
+
+# Location details
+xmin, xmax = 116.25, 116.5
+ymin, ymax = 39.75, 40.1
 
 window = data[(xmin < data.longitude) & (data.longitude < xmax) & \
             (ymin < data.latitude) & ( data.latitude < ymax)]
@@ -186,15 +177,17 @@ window = data[(xmin < data.longitude) & (data.longitude < xmax) & \
 x = np.array(window.longitude)
 y = np.array(window.latitude)
 
-plt.figure(figsize = (12,8), dpi=100)
-plt.hexbin(x,y, bins='log', gridsize=1000, cmap=plt.cm.hot)   # black -> red > white
+plt.figure(figsize = (10,6), dpi=150)
+plt.hexbin(x,y, bins='log', gridsize=800, cmap=plt.cm.hot)   # black -> red > white
 plt.axis([xmin, xmax, ymin, ymax])
-plt.title("Traffic data for Beijing -- 5th Ring Road")
-cb = plt.colorbar()
-#cb = plt.colorbar()
+plt.title("Traffic data for Beijing's 5th Ring Road")
+plt.xlabel('Longitude (degrees)')
+plt.ylabel('Latitude (degrees)')
 
-cb.set_label('Number of points (log10)')
+cb = plt.colorbar(format=LogFormatterHB())
+cb.set_label('Number of points')
 
+plt.tight_layout()
 plt.show()
 
 
@@ -202,10 +195,39 @@ plt.show()
 one_taxi = data[data.taxi_id == 2172]
 
 
+
+
+
 #plt.figure(figsize = (12,8), dpi=100)
 
 #%% Plot
-plt.plot(np.array(one_taxi.longitude), np.array(one_taxi.latitude))
+plt.plot(my_low_pass_filter(np.array(one_taxi.longitude)),\
+        my_low_pass_filter(np.array(one_taxi.latitude)))
 plt.axis([xmin, xmax, ymin, ymax])
 plt.title("Traffic data for Beijing -- 5th Ring Road")
 plt.show()
+
+#%% Kalman Filter and plot results
+
+#kf = KalmanFilter(transition_matrices = [[1, 1], [0, 1]], observation_matrices = [[0.1, 0.5], [-0.3, 0.0]])
+#kf = KalmanFilter(transition_matrices = [[1, 0], [0, 1]], observation_matrices = [[0.1, 0.5], [-0.3, 0.0]], n_dim_obs=2, n_dim_state=2)
+measurements = np.asarray([one_taxi.longitude, one_taxi.latitude])
+measurements = measurements.T[0:400]
+kf = KalmanFilter(initial_state_mean = measurements[0], \
+           n_dim_obs=2, n_dim_state=2)
+
+
+kf = kf.em(measurements)
+#(filtered_state_means, filtered_state_covariances) = kf.filter(measurements)
+(smoothed_state_means, smoothed_state_covariances) = kf.smooth(measurements)
+
+#plt.plot(filtered_state_means.T[0],filtered_state_means.T[1])
+plt.plot(smoothed_state_means.T[0],smoothed_state_means.T[1])
+plt.title("Smoothed Data With Karman Filter")
+plt.xlabel('Longitude (degrees)')
+plt.ylabel('Latitude (degrees)')
+print "Taxi data for Taxi ID: ", 2172
+print "Start date and time: ", one_taxi.date_time[0]
+print "Duration :", one_taxi.date_time[400] - one_taxi.date_time[0]
+plt.figure()
+#plt.plot(measurements.T[0],measurements.T[1])
